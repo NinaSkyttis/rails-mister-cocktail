cd65af1840d4f58f25d69bb3ea8a5de34c025bca
Nina Skyttmo <nina@dynur.com>
2020-03-13 16:10:47 +0100

    yesyes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
-puts "destroy all database"
-Ingredient.destroy_all
-Cocktail.destroy_all
-Dose.destroy_all
-require 'open-uri'
-require 'json'
-url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-ingredient_serialized = open(url).read
-parsed_ingredients = JSON.parse(ingredient_serialized)
-array_of_ingredients = parsed_ingredients['drinks']
-array_of_ingredients.each do |ingredient_hash|
-ingredient = Ingredient.new(
-  name: ingredient_hash['strIngredient1']
-  )
-ingredient.save!
-end
+# puts "destroy all database"
+# Ingredient.destroy_all
+# Cocktail.destroy_all
+# Dose.destroy_all
+# require 'open-uri'
+# require 'json'
+# url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# ingredient_serialized = open(url).read
+# parsed_ingredients = JSON.parse(ingredient_serialized)
+# array_of_ingredients = parsed_ingredients['drinks']
+# array_of_ingredients.each do |ingredient_hash|
+# ingredient = Ingredient.new(
+#   name: ingredient_hash['strIngredient1']
+#   )
+# ingredient.save!
+# end
 # cocktail = Cocktail.create!(name: "caipirinha")
 # dose = Dose.new
 # dose.description = "200ml"
@@ -37,15 +37,15 @@ end
 
 
 
-# mint_julep = Cocktail.create!(name: "Mint Julep")
-# whiskey = Cocktail.create(name: "Whiskey Sour")
-# mojito = Cocktail.create(name: "Mojito")
-# lemon = Ingredient.create(name: "lemon")
-# ice = Ingredient.create(name: "ice")
-# mint = Ingredient.create(name: "mint leaves")
-# Dose.create(description: "6", ingredient: mint, cocktail: mint_julep)
-# Dose.create(description: "2 oz", ingredient: ice, cocktail: whiskey)
-# Dose.create(description: "1 tsp", ingredient: lemon, cocktail: mojito)
+mint_julep = Cocktail.create!(name: "Mint Julep")
+whiskey = Cocktail.create(name: "Whiskey Sour")
+mojito = Cocktail.create(name: "Mojito")
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+mint = Ingredient.create(name: "mint leaves")
+Dose.create(description: "6", ingredient: mint, cocktail: mint_julep)
+Dose.create(description: "2 oz", ingredient: ice, cocktail: whiskey)
+Dose.create(description: "1 tsp", ingredient: lemon, cocktail: mojito)
 
 
 
