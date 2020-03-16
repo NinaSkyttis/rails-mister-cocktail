1b3e7e6bc95f250a9c78c2abbbad549e05144105
Nina Skyttmo <nina@dynur.com>
2020-03-16 08:46:32 +0100

    front end work

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,6 +2,7 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
   end
+  #added parameter to see if I can display images. Take it away if it doesn't work.
 
   def show
     @cocktail = Cocktail.find(params[:id])
@@ -26,13 +27,15 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path(@cocktails)
+  end
+
   private
 
   def cocktail_params
     params.require(:cocktail).permit(:name, :photo)
   end
 end
-
-
-
-
